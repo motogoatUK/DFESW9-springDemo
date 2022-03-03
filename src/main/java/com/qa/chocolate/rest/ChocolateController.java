b8d571fbcf45e9624c8a53eed5137a976a24d209
Simon Thornes <software@motogoat.co.uk>
2022-03-03 12:59:22 +0000

    done

--- src/main/java/com/qa/chocolate/rest/ChocolateController.java
+++ src/main/java/com/qa/chocolate/rest/ChocolateController.java
@@ -3,6 +3,8 @@ package com.qa.chocolate.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,14 +21,15 @@ public class ChocolateController {
 
 	// Create -POST
 	@PostMapping("/createChoco")
-	public void createChoco(@RequestBody Chocolates c) {
+	public ResponseEntity<Chocolates> createChoco(@RequestBody Chocolates c) {
 		this.choco.add(c);
+		return new ResponseEntity<Chocolates>(c,HttpStatus.CREATED);
 	}
 
 	// Read - GET
 	@GetMapping("/getChoco")
-	public List<Chocolates> getChoco() {
-		return this.choco;
+	public ResponseEntity<List<Chocolates>> getChoco() {
+		return new ResponseEntity<List<Chocolates>>(this.choco,HttpStatus.I_AM_A_TEAPOT);
 	}
 	//get by Id
 	@GetMapping("/getOne/{id}")
@@ -49,8 +52,9 @@ public class ChocolateController {
 	
 	// Delete - DELETE
 	@DeleteMapping("/removeChoco/{id}")
-	public Chocolates removeChoco(@PathVariable int id) {
-		return this.choco.remove(id);
+	public ResponseEntity<Chocolates> removeChoco(@PathVariable int id) {
+		var del = this.choco.remove(id);
+		return new ResponseEntity<Chocolates>(del,HttpStatus.NO_CONTENT);
 	}
 	
 }
